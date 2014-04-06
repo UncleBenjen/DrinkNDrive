3c97d984663aacdfc816667e70d2359e2e0baf9e
marekfs <msiekier@uwo.ca>
2014-04-06 15:49:56 -0400

    Able to move vehicle left to right

---
+++ project/Assets/player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	private float steer = 0;
+	private float drunk = 0;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 eulerAngles = transform.rotation.eulerAngles;
+		eulerAngles = new Vector3(0, eulerAngles.y, 0);
+		transform.rotation = Quaternion. Euler(eulerAngles);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetInput();
+	}
+
+	void GetInput()
+	{
+		steer = -Input.GetAxis("Horizontal");
+	}
+
+	void ApplySteering()
+	{
+		//transform.RotateAround (Vector3.zero, Vector3.up, 20 * steer);
+		rigidbody.position += Vector3.forward * steer;
+		
+	}
+
+	void FixedUpdate()
+	{	
+		ApplySteering();
+	}
+}
