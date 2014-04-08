139c9325b34f91237e7920e009c3b025c899489e
Ben Speir <benjaminspeir@gmail.com>
2014-04-07 22:13:58 -0400

    added gui text for obstacles & time. Darkened scene; added street lights and headlamps

--- project/Assets/Scripts/destroy.cs
+++ project/Assets/Scripts/destroy.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class destroy : MonoBehaviour {
 	public int obstacle_passed =0;
 	public bool hard_mode = false;
+
 	// Use this for initialization
 	void Start () {
 	
---
+++ project/Assets/Scripts/driveGUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class driveGUI : MonoBehaviour {
+	public bool gui_is_on = true;
+
+	// Use this for initialization
+	public GUIText targetGuiText;
+	public GUIText time_gui;
+	public GameObject back;
+	public GameObject obstacles;
+	public GameObject time;
+
+	public DateTime start;
+	public DateTime delta;
+
+	void Start()
+	{
+		obstacles = GameObject.Find("obstacle_text");
+		targetGuiText=obstacles.GetComponent<GUIText>();
+		time = GameObject.Find ("time_text");
+		time_gui = time.GetComponent<GUIText>();
+		back = GameObject.Find("back");
+
+		start = System.DateTime.Now;
+	}
+	// Update is called once per frame
+	void Update () {
+		time_gui.text = (System.DateTime.Now - start).ToString();
+		targetGuiText.text = back.GetComponent<destroy>().obstacle_passed.ToString();
+	}
+
+	void OnGUI()
+	{
+
+		
+	}
+}
